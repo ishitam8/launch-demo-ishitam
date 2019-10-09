efb5b0fe48eca1466f13e9b8de0a09ac1560adba
Ishita Mehta <ishitam@microsoft.com>
2019-10-09 12:36:51 +0530

    Test new tag related yaml changes

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         System.out.println("New line changed code added");
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot! CReating and testing a new tag! Also the PR workflow!";
     }
 	
 	@RequestMapping("/logout")
