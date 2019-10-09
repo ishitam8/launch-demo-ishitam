a36bd09186621d131c6508e55430d3e775b65230
Ishita Mehta <43136080+ishitam8@users.noreply.github.com>
2019-10-09 19:11:33 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         System.out.println("New line changed code added");
-        return "Greetings from Spring Boot!, Lets test PR workflow now!";
+        return "Greetings from Spring Boot!, Lets test PR workflow now! Raising a new PR!";
     }
 	
 	@RequestMapping("/logout")
