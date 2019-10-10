9c03784c686c959b35b78d52602c7422e6cccc29
Ishita Mehta <43136080+ishitam8@users.noreply.github.com>
2019-10-10 13:24:44 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         System.out.println("New line changed code added");
-        return "Test PR open flow. With new init";
+        return "Test PR open flow. With new initFailed again";
     }
 	
 	@RequestMapping("/logout")
